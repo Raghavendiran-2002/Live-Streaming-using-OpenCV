6da7393967e61045fefc704ce5ef3bd3c3c070b5
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2022-10-07 22:37:34 +0530

    Basic-Auth

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response,  redirect, url_for, request
+
+import requests
 import cv2
 
 app = Flask(__name__)
@@ -6,6 +8,7 @@ app = Flask(__name__)
 camera = cv2.VideoCapture(0)  # use 0 for web camera
 #  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 # for local webcam use cv2.VideoCapture(0)
+
 
 def gen_frames():  # generate frame by frame from camera
     while True:
@@ -20,17 +23,30 @@ def gen_frames():  # generate frame by frame from camera
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            # return redirect(url_for('home'))
+            return render_template('index.html')
+    return render_template('login.html', error=error)
+
+
 @app.route('/video_feed')
 def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
+    # Video streaming route. Put this in the src attribute of an img tag
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/')
 def index():
     """Video streaming home page."""
-    return render_template('index.html')
+    return "HI"
+    # return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8004,host = "172.20.10.4")
+    app.run(debug=True, port=8004)  # host="172.20.10.4")
